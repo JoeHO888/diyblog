78360ab8c046348ed5f9b3a1558d4103d2f03c45
JoeHO888 <joe.hochotai@gmail.com>
2019-05-12 09:56:56 +0800

    Add Procfile, Change Settings

--- diyblog/settings.py
+++ diyblog/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
+import dj_database_url
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -126,3 +127,7 @@ DEBUG = True
 ALLOWED_HOSTS = []
 EMAIL_HOST_USER = 'blogjoeho@gmail.com'
 EMAIL_HOST_PASSWORD = '!Pass123'
+
+
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
